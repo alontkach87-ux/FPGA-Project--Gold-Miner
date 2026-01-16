a11e3d58173d2c1ed64a35eb2f729f9ba2381981
Yanay <yanai2000@gmail.com>
2026-01-16 22:34:39 +0200

    fix gender swap logic

--- RTL/VGA/MaleFemaleMux.sv
+++ RTL/VGA/MaleFemaleMux.sv
@@ -1,26 +1,25 @@
 module MaleFemaleMux (
-    input  logic clk,
-    input  logic resetN,
-    input  logic flag,
-    output logic out
+    input logic clk,
+    input logic resetN,
+    input logic flag, // input from game controller
+    output logic state // 1 for female, 0 for male
 );
 
-    logic state;
+    logic flag_delayed; // History register
 
     always_ff @(posedge clk or negedge resetN) begin
-        if (!resetN) begin
-            state <= 1'b1; // Default is 1 - Female
+        if(!resetN) begin
+            state <= 1'b1;       // Default: Female
+            flag_delayed <= 1'b0;
         end
         else begin
-            if (flag == 1'b1 && state == 1'b1) begin //Male
-                state <= 1'b0;
-            end
-            else if (flag == 1'b1 && state == 1'b0) begin
-                state <= 1'b1;
+            flag_delayed <= flag; // Remember value from previous cycle
+            
+            // RISING EDGE DETECTOR:
+            if(flag == 1'b1 && flag_delayed == 1'b0) begin
+                state <= !state; // Toggle EXACTLY once
             end
         end
     end
-
-    assign out = state;
 
 endmodule
